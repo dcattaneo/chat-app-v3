bed214777c1b564c830a75f040f355319f8fb49b
dcattaneo <diegocattaneo@hotmail.com.ar>
2024-09-12 10:35:46 -0300

    db connection issues fixed

--- src/app/api/check-db/route.ts
+++ src/app/api/check-db/route.ts
@@ -1,14 +1,16 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { connectDB } from "@/app/lib/db";
 
 export async function GET() {
-    try {
-        await connectDB()
-        console.log('DB Connected')
-        return NextResponse.json({ message: 'Database connected successfully' })
-
-    } catch (error) {
-        console.error('DB connection error:', error)
-        return NextResponse.json({ message: 'Failed to connect to the database' }, { status: 500 });
-    }
+  try {
+    await connectDB();
+    console.log("DB Connected");
+    return NextResponse.json({ message: "Database connected successfully" });
+  } catch (error) {
+    console.error("DB connection error:", error);
+    return NextResponse.json(
+      { message: "Failed to connect to the database" },
+      { status: 500 }
+    );
+  }
 }
