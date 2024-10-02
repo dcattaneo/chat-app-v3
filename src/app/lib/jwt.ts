6a03b433cc056a959428a534a073e98cb11b9f77
dcattaneo <diegocattaneo@hotmail.com.ar>
2024-10-02 14:39:38 -0300

    jwt lib

--- src/app/lib/jwt.ts
+++ src/app/lib/jwt.ts
@@ -1,17 +1,15 @@
-import jwt from 'jsonwebtoken'
-
+import jwt from "jsonwebtoken";
 
 export function createAccessToken(payload: object): Promise<string> {
-    return new Promise((resolve, reject) => {
-        jwt.sign(
-            payload,
-            process.env.PRIVATE_KEY || 'secret',
-            { expiresIn: "1d" },
-            (err, token) => {
-                if (err) reject(err)
-                if (token) resolve(token)
-            }
-        )
-    })
+  return new Promise((resolve, reject) => {
+    const secretKey = process.env.PRIVATE_KEY;
 
+    if (!secretKey) {
+      return reject(new Error("JWT secret key is missing"));
+    }
+    jwt.sign(payload, secretKey, { expiresIn: "1d" }, (err, token) => {
+      if (err) reject(err);
+      if (token) resolve(token);
+    });
+  });
 }
