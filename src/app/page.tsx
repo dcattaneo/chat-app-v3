0802f2ac74fc6c5b80f799149e7e1d4fd6c59c79
dcattaneo <diegocattaneo@hotmail.com.ar>
2024-10-02 14:44:22 -0300

    main page with redirection in case of lack of credentials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import { useAuth } from "./auth-context/hooks/useAuth";
+import { useGetUsers } from "./hooks/useGetUsers";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <div className="">Hi</div>;
+  const { user } = useAuth();
+  const router = useRouter();
+  const { data, isError, error, isLoading } = useGetUsers();
+  console.log(user);
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/sign-in");
+    }
+  }, [user, router]); // Only run this effect if user or router changes
+
+  return <div className="w-screen h-screen">Home</div>;
 }
