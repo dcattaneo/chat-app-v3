fbfde9046f3218ea9923f9e892358185b3c87a86
dcattaneo <diegocattaneo@hotmail.com.ar>
2024-10-02 14:40:00 -0300

    dependencies added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        title: ["Adventure Time Logo"],
+      },
     },
   },
   plugins: [],
